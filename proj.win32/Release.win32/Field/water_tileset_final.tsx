763594d19df9ef4996c2c558db1afb91937df259
maksymbazala <maximbazala@gmail.com>
2019-03-06 23:39:52 +0200

    win32 compatibility issues fix

---
+++ proj.win32/Release.win32/Field/water_tileset_final.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="water_tileset_final" tilewidth="80" tileheight="80" tilecount="100" columns="10">
+ <image source="field_bg.png" width="800" height="800"/>
+</tileset>
